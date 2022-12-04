b219df8f5753df9ebca3da0facbfe96d8273f3f8
Yan Shcherbakov <yan.shcherbakov@gmail.com>
2022-12-04 17:47:37 +0100

    - Working Clipper - Doesn't work well in stereo - For some reason blends clean signal when heavily driven

--- source/NonInvertingOpAmpClipper.h
+++ source/NonInvertingOpAmpClipper.h
@@ -1,3 +1,17 @@
+/*
+ * TODOs
+ * 1. I'll digitise the Non-Inverting Op Amp Clipper Circuit
+ * 2. Mark variables as constants
+ * 2. Fix the warning for explicit conversion to float
+ * 2. Refactor Diodes into a function
+ * 3. Refactor the reset method
+ * 4. Refactor base class for processor
+ *
+ * At each step make sure it works
+ */
+
+#pragma once
+
 class NonInvertingOpAmpClipper
 {
 public:
@@ -48,8 +62,61 @@ private:
 	float Fs = 44100.f; // Initial sample rate
 	float Ts = 1.f / Fs;
 
+	// Components
+	const float eta = 1.f;
+	const float Is = 1e-15;
+	const float Vt = 26e-3;
+
+	float C1 = 47e-9;
+	float R1 = Ts / (2.f * C1);
+	float R4 = 4700.f;
+
+	float C2 = 51e-12;
+	float R2 = Ts / (2.f * C2);
+	float R3 = 51000.f + 500e3;
+
+	// Combined Resistances
+	float G1 = (1.f + R4 / R1);
+	float G4 = (1.f + R1 / R4);
+
+	// States
+	float X1 = 0.f;
+	float X2 = 0.f;
+	float Vd = 0.f;
+
+	float thr = 0.00000000001f; // Might want to make const
+
 	float processSingleSample(float Vin) {
-		float Vout = Vin;
+		int iter = 1; // Change to size_t
+		float b = 1.f;
+
+		float p = -Vin / (G4 * R4) + R1 / (G4 * R4) * X1 - X2;
+
+		float fVd = p + Vd / R2 + Vd / R3 + 2.f*Is * sinh(Vd / (eta * Vt));
+
+		while (iter < 50 && abs(fVd) > thr)
+		{
+			float fpVd = 2.f * Is / (eta * Vt) * cosh(Vd / (eta * Vt)) + 1.f / R2 + 1.f / R3;
+			float Vnew = Vd - b * fVd / fpVd;
+			float fn = p + Vnew / R2 + Vnew / R3 + 2.f * Is * sinh(Vnew / (eta * Vt));
+
+			if (abs(fn) < abs(fVd))
+			{
+				Vd = Vnew;
+				b = 1.f;
+			}
+			else
+			{
+				b *= 0.5f;
+			}
+
+			fVd = p + Vd / R2 + Vd / R3 + 2.f * Is * sinh(Vd / (eta * Vt));
+			iter++;
+		}
+
+		float Vout = Vd + Vin;
+		X1 = (2.f / R1) * (Vin / G1 + X1 * R4 / G1) - X1;
+    	X2 = (2.f / R2) * (Vd) - X2;
 
 		return Vout;
 	}
@@ -57,6 +124,15 @@ private:
 	void updateCoefficients()
 	{
 		Ts = 1.f / Fs;
+
+		R1 = Ts / (2.f * C1);
+		R4 = 4700.f; // Might want to remove and make const
+
+		R2 = Ts / (2.f * C2);
+		R3 = 51000.f + 500e3; // Might want to remove and make const
+
+		G1 = (1.f + R4 / R1);
+		G4 = (1.f + R1 / R4);
 	}
 
 	//==============================================================================
